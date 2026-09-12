651a0e3c9c65c083ce1f77e4d478ea3b741ac11a
Harshit <heroharshitlaptop@gmail.com>
2026-09-12 22:56:30 +0530

    Optimised code base

--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -3,12 +3,7 @@
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+import { cn } from "@/lib/utils"
 
 const Sheet = SheetPrimitive.Root
 
